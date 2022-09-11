cc6ef6f249ad2898c25fd297f5d090352c5ae766
Guillaume <guillaume.chatriot@gmail.com>
2022-09-11 14:38:13 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import allFoods from './foods.json';
+import React, { useState } from 'react';
+import FoodBox from './components/FoodBox';
 import './App.css';
-import foods from './foods.json';
-// import { Card, Row, Col, Divider, Input, Button } from 'antd';
-import { useState } from 'react';
+import AddFoodForm from './components/AddFoodForm';
+import Search from 'antd/lib/transfer/search';
+import { Divider } from 'antd';
 
 function App() {
-  const [foodList, setFoodlist] = useState(foods);
-  return <h1> Food List </h1>
-  foodList.map
+  const [foodList, setFoodList] = useState(allFoods);
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    calories: '',
+    servings: '',
+  });
+
+  return (
+    <div className="App">
+      <AddFoodForm
+        formData={formData}
+        setFormData={setFormData}
+        foodList={foodList}
+        setFoodList={setFoodList}
+      />
+      <Search foodList={foodList} setFoodList={setFoodList} />
+      <Divider>Food List</Divider>
+      <div className="foodListContainer">
+        {foodList.map((food) => {
+          return (
+            <FoodBox
+              key={food.name}
+              food={food}
+              foodList={foodList}
+              setFoodList={setFoodList}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
 export default App;
